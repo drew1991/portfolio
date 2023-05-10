f7df1d2bd8ca473f61442533db34147409d4bcf5
Andrew <mr.onyanchaandrew@gmail.com>
2023-05-10 07:24:08 +0300

    more changes

---
+++ build/precache-manifest.a5bd874425a4894fe8edee4c5a73c918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1efe2baddba04b41f6a8",
+    "url": "/static/css/main.5db9168a.chunk.css"
+  },
+  {
+    "revision": "1efe2baddba04b41f6a8",
+    "url": "/static/js/main.ab8467b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f4567ec54669d8376b2",
+    "url": "/static/js/2.618b9d28.chunk.js"
+  },
+  {
+    "revision": "6a569a7df82567e069cde26e1aab7f09",
+    "url": "/static/media/drew2.6a569a7d.png"
+  },
+  {
+    "revision": "7797a4e16ea4af7e56f110f5b8784cbc",
+    "url": "/index.html"
+  }
+];
